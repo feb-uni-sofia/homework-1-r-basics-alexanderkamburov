5efaea5a83689f2086637919b030b033be1bd41d
Alexander Kamburov <office@alexanderkamburov.com>
2018-04-02 11:53:48 +0300

    After many trials and trepidations, after veering to the brink of failure and humiliation, this homework, hopefully, is finished. There are many great people who made this possible and I would like to thank a few: Soren Kierkegaard, Sonny Liston, Gregor Mendel - the founder of the science of genetics, my tailor, the London Symphony Orchestra brass section, the Royal Society for the prevention of birds, The planet Saturn, and of course all of its rings and last but not least, mum.

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,71 @@
+# a) a.k.a. hot 'n heavy
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+# sounds like a terrible aftershave
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+dates <- c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+
+# b)
+diffTemp <- xmax - xmin
+diffTemp
+
+# c)
+meanXmin <- mean(xmin)
+meanXmin
+# a terrible aftershave with a fancy label
+meanXmax <- mean(xmax)
+meanXmax
+
+# d)
+xmin[xmin < meanXmin]
+
+# e) Some ambiguity in the question:
+# what I assume the question means: the maximum values above the average for the maximums
+xmax[xmax > meanXmax]
+# what a possible interpretation is min temp' whose 'indices' correspond to the indices in
+# xmax above the average
+xmin[xmax > meanXmax]
+
+
+# f) "Name the members" but not said which names to use! Hm...
+# First try: Days of week (boring)
+names(xmax) <- dates
+names(xmin) <- dates
+# Second try: Princeps name rating (awesome)
+names(xmax) <- c('Gaius', 'Octavius', 'Imperator', 'Caesar', 'Divi', 'Filius', 'Augustus')
+names(xmin) <- c('Gaius', 'Julius', 'Caesar', 'Augustus', 'Germanicus', 'Caligula', 'The nasty')
+
+
+# g) ǝıן ɐ sı ǝɯɐɹɟ ǝɥʇ
+temperatures <- data.frame(
+  xmin = xmin,
+  xmax = xmax
+)
+
+temperatures
+
+# h) Kelvin > Fahrenheit
+temperatures$xminFahrenheit <- xmin * 9/5 + 32
+temperatures
+
+# i)
+onlyTheTemperatureMeasurementsInFahrenheit <- data.frame(
+  measurements = temperatures$xminFahrenheit
+)
+
+onlyTheTemperatureMeasurementsInFahrenheit
+
+# j)
+#   i)
+onlyTheTemperatureMeasurementsInFahrenheitMonFri <- data.frame(
+  measurements = onlyTheTemperatureMeasurementsInFahrenheit[(1:5), ]
+)
+onlyTheTemperatureMeasurementsInFahrenheitMonFri
+
+#   ii)
+onlyTheTemperatureMeasurementsInFahrenheitMonFriV2 <- data.frame(
+  measurements = onlyTheTemperatureMeasurementsInFahrenheit[-(6:7), ]
+)
+
+onlyTheTemperatureMeasurementsInFahrenheitMonFriV2
+
 
